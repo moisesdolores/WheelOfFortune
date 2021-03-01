b5b206c43e56d746c636bd54ef09eb5df936431d
Moises Dolores <moisesdolores96@gmail.com>
2021-02-28 20:48:21 -0500

    final

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 "use strict";
 import { getRandomWord } from "./randomWord.js";
 
+const form = document.getElementById("form");
 const startButton = document.getElementById("start");
+const submitButton = document.getElementById("submit");
+const fullWordButton = document.getElementById("full-submit");
 const hiddenWord = document.getElementById("word");
+const alreadyGuessed = document.getElementById("alreadyGuessed");
 let randWord = getRandomWord();
-let testWord = "";
+let displayWord = "";
 
 function hide() {
-  testWord = "";
+  displayWord = "";
   for (let i of randWord) {
-    testWord += "-";
+    displayWord += "-";
   }
 }
 
 function newWord() {
   randWord = getRandomWord();
   randWord = randWord.toUpperCase();
-  //   console.log(randWord);
+  console.log(randWord);
   hide();
-  hiddenWord.innerText = testWord;
+  hiddenWord.innerText = displayWord;
 }
 
 function showWord() {
@@ -28,40 +32,70 @@ function showWord() {
   for (let i in randWord) {
     let ele = randWord[i];
     if (guess.includes(ele)) {
-      testWord = testWord.split("");
-      testWord[i] = ele;
-      testWord = testWord.join("");
+      displayWord = displayWord.split("");
+      displayWord[i] = ele;
+      displayWord = displayWord.join("");
     }
   }
+  hiddenWord.innerText = displayWord;
+}
 
-  hiddenWord.innerText = testWord;
+function fullWordCheck() {
+  let guess = document.getElementById("fullGuess").value;
+  guess = guess.toUpperCase();
+  const letters = /^[A-Za-z]+$/;
+  if (guess.match(letters)) {
+    if (guess === randWord) {
+      alert("You are Correct!");
+      hiddenWord.innerText = randWord;
+    } else {
+      alert("You are Incorrect");
+      newWord();
+      alreadyGuessed.innerHTML = "";
+    }
+  }
+}
+
+function previouslyGuessed() {
+  let guess = document.getElementById("myGuess").value;
+  const letters = /^[A-Za-z]+$/;
+  if (guess.match(letters)) {
+    guess = guess.toUpperCase();
+    const li = document.createElement("li");
+    li.append(guess);
+    alreadyGuessed.appendChild(li);
+  }
+}
+
+function validate() {
+  const myGuess = document.forms["form"]["myGuess"].value;
+  const fullGuess = document.forms["form"]["fullGuess"].value;
+  const letters = /^[A-Za-z]+$/;
+  if (myGuess.match(letters) || fullGuess.match(letters)) {
+    return true;
+  } else {
+    alert("Enter a Valid Guess");
+    return false;
+  }
 }
 
 startButton.addEventListener("click", (event) => {
   event.preventDefault();
   newWord();
+  alreadyGuessed.innerHTML = "";
+  document.getElementById("myGuess").focus();
 });
 
-const submitButton = document.getElementById("submit");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validate();
+  form.reset();
+});
+
 submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
   showWord();
-  document.getElementById("form").reset();
+  previouslyGuessed();
 });
-
-function fullWordCheck() {
-  let guess = document.getElementById("fullGuess").value;
-  guess = guess.toUpperCase();
-  if (guess === randWord) {
-    alert("You are Correct!");
-    hiddenWord.innerText = randWord;
-  } else {
-    alert("You are Incorrect");
-  }
-}
-const fullWordButton = document.getElementById("full-submit");
 fullWordButton.addEventListener("click", (event) => {
-  event.preventDefault();
   fullWordCheck();
-  document.getElementById("form").reset();
 });
